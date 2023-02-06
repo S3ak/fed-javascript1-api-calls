ba91330ed1da9733c29238b7a360c68a36eec96f
Monde Sineke <nsineke@gmail.com>
2023-02-06 22:57:38 +0100

    feat(css): ✨ Improve UI

--- libs/nav.js
+++ libs/nav.js
@@ -22,37 +22,53 @@ class Nav extends HTMLElement {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
       <style>
-        :host {
-          display: block;
-          padding: 10px;
-        }
-
         .is-active {
           color: green;
         }
 
         nav {
+          padding: 1em 1.2em;
           display: flex;
-          gap: 2.5em;
+          flex-direction: column;
+          gap: .5em;
           flex-wrap: wrap;
+          background: var(--light-grey, transparent);
         }
 
-        nav a {
+        a {
           min-height: 34px;
+          text-decoration: none;
+          text-transform: capitalize;
+          color: var(--grey, #333);
+        }
+
+        summary {
+          background: var(--light-grey, transparent);
+          border: none;
+          border-radius: 5px;
+          padding: 0.5em 1em;
+          color: var(--grey, #333);
+          cursor: pointer;
         }
       </style>
 
-      <nav>
-        <a href="/">Home</a>
-        <a href="/examples.html">Examples</a>
-        <a href="/examples/retrieving-parameters.html">retrieving-parameters</a>
-        <a href="/examples/adding-parameters-list.html">adding-parameters-list</a>
-        <a href="/examples/adding-parameters-details.html">adding-parameters-details</a>
-        <a href="/examples/reusable-html-function.html">reusable-html-function</a>
-        <a href="/examples/get-requests-handling-errors.html">get-requests-handling-errors</a>
-        <a href="/examples/try-catch-finally.html">try-catch-finally</a>
-        <a href="/examples/then-catch.html">then-catch</a>
-      <nav>
+      <details>
+        <summary>
+          Navigation
+        </summary>
+        
+        <nav>
+          <a href="/">Home</a>
+          <a href="/examples.html">Examples</a>
+          <a href="/examples/retrieving-parameters.html">retrieving-parameters</a>
+          <a href="/examples/adding-parameters-list.html">adding-parameters-list</a>
+          <a href="/examples/adding-parameters-details.html">adding-parameters-details</a>
+          <a href="/examples/reusable-html-function.html">reusable-html-function</a>
+          <a href="/examples/get-requests-handling-errors.html">get-requests-handling-errors</a>
+          <a href="/examples/try-catch-finally.html">try-catch-finally</a>
+          <a href="/examples/then-catch.html">then-catch</a>
+        </nav>
+      </details>
     `;
 
     const currentUrl = window.location.pathname;
